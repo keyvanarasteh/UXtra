2df6f8ddcc07ed65f9244c99c8aeb3b5edf98bd5
zeynepdurmus <zeynep.durmus@spike.com.tr>
2022-12-28 17:21:12 +0300

    widget

---
+++ uxtra/lib/widgets/sign_in_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: Text("helloworld"),
+        ),
+      ],
+    );
+  }
+}
