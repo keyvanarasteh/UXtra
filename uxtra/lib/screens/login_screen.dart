eb9d70ca1b84592492df6b95fd658bb387f0bb29
zeynepdurmus <zeynep.durmus@spike.com.tr>
2023-01-06 11:48:10 +0300

    sign in widget created

--- uxtra/lib/screens/login_screen.dart
+++ uxtra/lib/screens/login_screen.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:uxtra/widgets/appbar.dart';
+
+import '../widgets/sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,8 +20,18 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Column(
         children: [
           appbar(),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Icon(Icons.ac_unit_sharp),
+          ),
+          //platform login button
+          sign_in(),
+          sign_in(),
+          sign_in(),
+          sign_in(),
         ],
       ),
     );
   }
 }
+
---
+++ uxtra/lib/widgets/sign_in.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class sign_in extends StatelessWidget {
+  const sign_in({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 56, 52, 52),
+          borderRadius: BorderRadius.all(Radius.circular(5))
+        ),
+        height: 35,
+        width: 250,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.search,
+              size: 15,
+              ),
+            ),
+            Text("Sign in with Google",
+            style: TextStyle(
+              fontSize: 12,
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
